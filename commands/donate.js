215f4b119b90ac90eb61ab7e8650795a16c22777
Lxphere <36829703+iiLegendNeverSleepsx@users.noreply.github.com>
2019-10-13 16:49:32 -0400

    Update donate.js

--- commands/donate.js
+++ commands/donate.js
@@ -10,24 +10,24 @@ module.exports.run = async (bot, message, args) => {
       icon_url: client.user.avatarURL
     },
     title: "Donations",
-    description: "Donating to us can help us run ads in Trivago along with the devlopment of it. All donations go towards advertisements and development. Below you can find different types of donations. If you have purchased one of these, make sure to do !getroles in <#632179809581006868>",
+    description: "Donating to us can help us run ads in Trivago along with the devlopment of it. All donations go towards advertisements and development. Below you can find different types of donations. If you have purchased one of these, make sure to do !getroles in <#632179809581006868>!",
     fields: [{
         name: "Small Donation",
-        value: "This is a small donation which is 50 robux. You may purchase it [here](https://www.roblox.com/catalog/3705485022/TH-Small-Donation) - https://www.roblox.com/catalog/3705485022/TH-Small-Donation"
+        value: "This is a small donation which is 50 robux. You may purchase it [here](https://www.roblox.com/catalog/3705485022/TH-Small-Donation)"
       },
       {
 	name: "Medium Donation",
-        value: "This is a medium donation which is 150 robux. You may purchase it [here](https://www.roblox.com/catalog/3705486298/TH-Medium-Donation) - https://www.roblox.com/catalog/3705486298/TH-Medium-Donation"
+        value: "This is a medium donation which is 150 robux. You may purchase it [here](https://www.roblox.com/catalog/3705486298/TH-Medium-Donation)"
       },
       {
 	name: "Large Donation",
-        value: "This is a large donation which is 500 robux. You may purchase it [here](https://www.roblox.com/catalog/3705487361/TH-Large-Donation) - https://www.roblox.com/catalog/3705487361/TH-Large-Donation"
+        value: "This is a large donation which is 500 robux. You may purchase it [here](https://www.roblox.com/catalog/3705487361/TH-Large-Donation)"
     }, 
     ],
     timestamp: new Date(),
     footer: {
       icon_url: client.user.avatarURL,
-      text: "Trivago Hotels - Owned by Sally and Sam"
+      text: "Trivago Hotels © 2019 - Owned by Sally and Sam"
     }
   }
 });
