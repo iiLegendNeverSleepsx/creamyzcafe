8a5e204ebe6658985b7f62e62aaafc5f6c947a15
Lxphere <36829703+iiLegendNeverSleepsx@users.noreply.github.com>
2020-05-13 15:25:22 -0400

    Update setavatar.js

--- commands/setavatar.js
+++ commands/setavatar.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 module.exports.run = async (bot, message, args) => {
     const allowedid = ['297860875937906689', '293060399106883584'];
     if (allowedid.includes(message.author.id)) {
-	    bot.user.setAvatar(args[0]).then(() => {message.reply(`new avatar set to (URL) **${args[0]}**!`); bot.guilds.get('613883505675599872').channels.get('710194830126350461').send(`My avatar has been changed to (URL) **${args[0]}** by **${message.author.tag}**.`)})
+	    bot.user.setAvatar(args[0]).then(() => {message.reply(`My new avatar is now **${args[0]}**!`); bot.guilds.get('613883505675599872').channels.get('710194830126350461').send(`My avatar has been changed to (URL) **${args[0]}** by **${message.author.tag}**.`)})
 	    	.catch((error) => {message.reply(`I can not set my avatar because of \`${error}\``); bot.guilds.get('613883505675599872').channels.get('710194830126350461').send(`Set avatar command ran by **${message.author.tag}** failed with error: \`${error}\`.`)})
 	    
     }
